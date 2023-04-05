e5c2950f007bfda73ecff5bc31eff1cce173bde2
HYnllvo <gpalsu0812@naver.com>
2023-04-05 19:17:46 +0900

    first commit

--- mySpartaSns/settings.py
+++ mySpartaSns/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-p-pqxw0pj9ytw#jx$zb066q55#30^)9bg4-@cy7!1u8*)z43)r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
